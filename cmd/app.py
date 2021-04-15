a4d9b39fcc39ab99cc42ff395b4e54c86bfc48d0
RinkeshSante <rinkoosante@gmail.com>
2021-04-15 20:59:06 +0530

    added switch base  cycle

--- cmd/app.py
+++ cmd/app.py
@@ -11,7 +11,6 @@ def writeSerial(s, serial_obj=None):
 
 def SolveCommand(s):
     if s[:4] != 'amk#' or s[-1] != '!' or s[5] != '@':
-        # print(s[:3], '--', s[-1])
         # display popup message here
         return 'syntax error'
     cmd_no = s[4]
@@ -27,3 +26,48 @@ def CreateCommand(no, data=''):
 
 
 # print(CreateCommand(9, 'ok'))
+
+while True:
+    op = '''-------------------------------------
+    0: exit
+    1: setSQL
+    2: autoSQL
+    3: getPWM
+    4: setPWM
+    5: listen
+    6: send
+    '''
+    print(op)
+
+    c = int(input('Enter choice : '))
+
+    if c == 0:
+        break
+
+    elif c == 1:
+        pass
+
+    elif c == 2:
+        pass
+
+    elif c == 3:
+        pass
+
+    elif c == 4:
+        pass
+
+    elif c == 5:
+        cmd = readSerial()
+        no, data = SolveCommand(cmd)
+        if no == 5:  # check cmd from hardware
+            print('cmd error')
+        print(data)
+
+    elif c == 6:
+        msg = input('enter message : ')
+        cmd = CreateCommand(4, msg)  # check cmd from hardware
+        op = writeSerial(cmd)
+        print('op :', op)
+
+    else:
+        print('invalid command')
